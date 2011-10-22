31ffd113ec5b707429600b0ac792554725185d4b
ufssf <ainonic@gmail.com>
2011-10-22 15:49:09 +0900

    first commit

---
+++ tumblr_post_state_selector.user.js
@@ -0,0 +1,26 @@
+//==UserScript==
+//@name tumblr post state selector
+//@match http://www.tumblr.com/reblog/*
+//==/UserScript==
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+  $('#post_state').val('2');
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
+
+
